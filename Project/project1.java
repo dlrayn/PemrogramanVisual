a2a708568702cacd68cc6412760fd1c8e34e1e9f
adela <119375246+dlrayn@users.noreply.github.com>
2024-08-06 15:20:47 +0700

    Add files via upload
    modul 1 dan 2

---
+++ Project/project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class project1 {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        project project1=new project();
+        project1.setVisible(true);
+    }
+}
---
+++ project1/Coba1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Coba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Project1 coba1=new Project1();
+        coba1.setVisible(true);
+    }
+    
+}
